b3d1ab236d1c63e9c7be16cbaa52dfe7b617086a
atompkins <atompkins@users.noreply.github.com>
2023-05-25 20:20:56 +0100

    more rate limiting

--- src/getPage.js
+++ src/getPage.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const rateLimit = require('axios-rate-limit');
 
-const api = rateLimit(axios.create({ baseURL: 'https://guide.fallensword.com/' }), { maxRPS: 3 });
+const api = rateLimit(axios.create({ baseURL: 'https://guide.fallensword.com/' }), { maxRPS: 1 });
 
 function getPage(url) {
   return api.get(url);
